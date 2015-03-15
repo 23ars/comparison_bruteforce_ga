cefff4927da5cfcb2828856874d420b454574551
23ars <ardeleanasm@gmail.com>
2015-03-15 16:18:46 +0200

    not done brute force, but improved

--- BruteForceDLL/BruteForceDLL.cpp
+++ BruteForceDLL/BruteForceDLL.cpp
@@ -15,31 +15,36 @@ void BruteForce::bruteForce(std::string targetString)
 }
 
 void BruteForce::recurse(int threadNo, bool found, std::string targetString, std::string availableCharacters, int width, int position, std::string baseString)
-{	
-	for (size_t i = 0; i < availableCharacters.length()-1; i+=2)
+{
+
+
+	for (size_t i = 0; i < availableCharacters.length() - 1; i += 2)
 	{
-		
+
 		if (position < width - 1)
 		{
-			if (found==true)
-			{
-				std::cout << "String is:" << baseString.c_str();
-				std::cout << std::endl;
-				exit(0);
-			}
-			std::cout << "CHECKED:" << baseString << std::endl;
 			if (targetString.compare(baseString) == 0)
 			{
 				found = true;
+				std::cout << "String is:" << baseString.c_str();
+				std::cout << std::endl;
+				break;
+				//exit(1);
+			}
+
+			std::cout << "CHECKED:" << baseString << std::endl;
+			if (threadNo == 0)
+			{
+
+				recurse(threadNo, found, targetString, availableCharacters, width, position + 1, baseString + availableCharacters.at(i));
+			}
+			else
+			{
+
+				recurse(threadNo, found, targetString, availableCharacters, width, position + 1, baseString + availableCharacters.at(i + 1));
 
 			}
-			if (threadNo==0)
-				recurse(threadNo,found,targetString,availableCharacters, width, position + 1, baseString + availableCharacters.at(i));
-			else
-				recurse(threadNo,found,targetString, availableCharacters, width, position +1, baseString + availableCharacters.at(i+1));
+
 		}
-		
-		
-		
 	}
 }
